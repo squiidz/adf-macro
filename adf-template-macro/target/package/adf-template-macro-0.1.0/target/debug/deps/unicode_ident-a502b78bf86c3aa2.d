3283477de9502ec29896cf50caab5824b7400d0f
Jonathan <jon.chaput@gmail.com>
2023-04-24 13:25:10 -0400

    added gitignore

--- adf-template-macro/target/package/adf-template-macro-0.1.0/target/debug/deps/unicode_ident-a502b78bf86c3aa2.d
+++ adf-template-macro/target/package/adf-template-macro-0.1.0/target/debug/deps/unicode_ident-a502b78bf86c3aa2.d
@@ -1,8 +1,8 @@
-/home/squiidz/dev/rust/adf-template/adf-template-macro/target/package/adf-template-macro-0.1.0/target/debug/deps/unicode_ident-a502b78bf86c3aa2.rmeta: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
+/home/squiidz/dev/rust/adf-template/adf-template-macro/target/debug/deps/unicode_ident-a502b78bf86c3aa2.rmeta: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
 
-/home/squiidz/dev/rust/adf-template/adf-template-macro/target/package/adf-template-macro-0.1.0/target/debug/deps/libunicode_ident-a502b78bf86c3aa2.rlib: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
+/home/squiidz/dev/rust/adf-template/adf-template-macro/target/debug/deps/libunicode_ident-a502b78bf86c3aa2.rlib: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
 
-/home/squiidz/dev/rust/adf-template/adf-template-macro/target/package/adf-template-macro-0.1.0/target/debug/deps/unicode_ident-a502b78bf86c3aa2.d: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
+/home/squiidz/dev/rust/adf-template/adf-template-macro/target/debug/deps/unicode_ident-a502b78bf86c3aa2.d: /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs
 
 /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/lib.rs:
 /home/squiidz/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.8/src/tables.rs:
